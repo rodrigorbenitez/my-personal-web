5761be27c1ccd044bde4f433c155267e86fb6323
RODRIGO BENITEZ <rodrigorbenitez2288@gmail.com>
2020-06-04 21:51:30 -0300

    Changes in scripts

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     """Renders the home page."""
 
-    return render_template('index.html')
+    return render_template('bio.html')
 
 
 @app.route('/home')
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -11,24 +11,3 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     });
 });
-// Renders contents of new page in main view.
-function load_page(name) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `/${name}`);
- 
-    request.onload = () => {
-
-        const response = request.responseText;
-        document.querySelector('#content').innerHTML = response
-        // Push state to URL.
-        document.title = name;
-        history.pushState({ 'title': name, 'text': response }, name, name);
-    };
-    request.send();
-}
-// Update text on popping state.
-window.onpopstate = e => {
-    const data = e.state;
-    document.title = data.title;
-    document.querySelector('#content').innerHTML = data.text;
-};
